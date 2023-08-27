d20403fe1d4971b2a117beaffe56184386848978
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-08-28 02:37:57 +0600

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
+import { setting } from "./src/config.ts";
+
+export default defineConfig({
+ site: `${setting.site.url}` ? `${setting.site.url}` : "http://site.com",
+ base: `${setting.site.base_path}` ? `${setting.site.base_path}` : "/",
+ trailingSlash: `${setting.site.trailing_slash}` ? "always" : "never",
+ experimental: { assets: true, viewTransitions: true },
+ compressHTML: true,
+ integrations: [
+  tailwind({
+   config: {
+    applyBaseStyles: false,
+   },
+  }),
+  react(),
+  mdx(),
+  sitemap(),
+  robotsTxt({
+   sitemap: [
+    `${setting.site.siteLink}/sitemap-index.xml`,
+    `${setting.site.siteLink}/sitemap-0.xml`,
+   ],
+  }),
+ ],
+});
---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const setting = {
+ site: {
+  name: "",
+  url: "https://site.com",
+  base_path: "/",
+  trailing_slash: false,
+  SEO: {
+   default_title: "",
+   default_description: "",
+   default_author: "",
+   default_og_image: "",
+  },
+ },
+};
