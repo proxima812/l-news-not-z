98d418c11a2cf2887632e305d36170acdbb67165
Камиль Мирихан <samgold@MacBook-Air-Kamil.local>
2023-08-29 19:49:08 +0600

    httpds

--- src/lib/utils/sortFunctions.ts
+++ src/lib/utils/sortFunctions.ts
@@ -1,11 +1,8 @@
 // sort by date
 export const sortByDate = (array: any[]) => {
-  const sortedArray = array.sort(
-    (a:any, b:any) =>
-      new Date(b.data.date && b.data.date) -
-      new Date(a.data.date && a.data.date)
-  );
-  return sortedArray;
+ const sortedArray = array.sort(
+  (a: any, b: any) =>
+   new Date(b.data.date && b.data.date) - new Date(a.data.date && a.data.date)
+ );
+ return sortedArray;
 };
-
-
